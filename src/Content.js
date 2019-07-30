f851cf7cef79110b18fd4f1598756ef95139ee3a
Maksura Alam <maksura.alam@gmail.com>
2019-07-30 16:49:35 +0700

    map through flashcards

--- src/Content.js
+++ src/Content.js
@@ -4,13 +4,35 @@ import Flashcard from './Flashcard';
 
 class Content extends Component {
 	// Data
+	state = {
+
+		flashcards: [
+			{
+				id: 1,
+				word: 'Tony Russo',
+				image: '2 Jul 2019 - 9:09AM',
+				definition: 'Hello World',
+				sentence: 'Hey this is a sentence'
+			}, {
+				id: 2,
+				word: 'John Smith',
+				image: '2 Jul 2019 - 9:12AM',
+				definition: 'Hey, what up?',
+				sentence: 'This is another sentence that includes the word'
+			}
+		]
+	}
 	// Functions
 	// Render
 	render() {
 		return (
   <div id="content">
 <div id="flashcard">
-<Flashcard />
+{
+	this.state.flashcards.map((f) => {
+		return <Flashcard />
+	})
+}
 
 </div>
  </div>
