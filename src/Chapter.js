17e26f99513de3bb91b18d5a2a27305e821c490c
Maksura Alam <maksura.alam@gmail.com>
2019-07-30 15:03:22 +0700

    Created Chapter component and CSS

---
+++ src/Chapter.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+
+class Chapter extends Component {
+	// Data
+
+	// Functions
+	// Render
+	render() {
+		return (
+			<li> Chapter 1 </li>
+		)
+	}
+}
+
+export default Chapter
