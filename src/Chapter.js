eed8167a06990da7c3fe2d642f8a445fbffd14a1
Maksura Alam <maksura.alam@gmail.com>
2019-07-30 17:31:35 +0700

    connected chapter names to chapter content

--- src/Chapter.js
+++ src/Chapter.js
@@ -1,12 +1,25 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 class Chapter extends Component {
   // Data
 
   // Functions
+  logChapter = () => {
+    console.log(this.props.chapter);
+    axios
+      .get(`http://localhost:4000/api/chapter/${this.props.chapter._id}`)
+      .then(res => {
+        console.log(res);
+      })
+      .catch(err => {
+        console.log("err", err);
+      });
+  };
+
   // Render
   render() {
-    return <li>{this.props.chapter.name}</li>;
+    return <li onClick={this.logChapter}>{this.props.chapter.name}</li>;
   }
 }
 
