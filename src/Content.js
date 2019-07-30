cdbc701f9e48c80bc2091759f30aaf77a94b35c5
Maksura Alam <maksura.alam@gmail.com>
2019-07-30 11:31:58 +0700

    Added one card to content using codepen

--- src/Content.js
+++ src/Content.js
@@ -8,6 +8,22 @@ class Content extends Component {
 	render() {
 		return (
 			<div id="content">
+    <div className="container" ontouchstart="this.classList.toggle('hover');">
+      <div className="card">
+    <div className="card_front">
+      <h1 className="card-symbol">&#128628;</h1>
+    </div>
+
+    <div className="card_back">
+      <h1 className="card-symbol">&#128628;</h1>
+      <div className="card-text">
+        <p>The word <strong>ampersand</strong> is a corruption of the phrase "and per se & (and)", meaning "and intrinsically the word and (represented by the symbol &)".</p>
+      <p>The ampersand can be traced back to the 1st century A.D. and the Old Roman cursive, in which the letters E and T occasionally were written together to form a ligature.</p>
+      </div>
+    </div>
+
+  </div>
+</div>
 
 
 			</div>
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -8,7 +8,7 @@ class Sidebar extends Component {
 	render() {
 		return (
 			<div id="sidebar">
-
+         
 
 			</div>
 		)
