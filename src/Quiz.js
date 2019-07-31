f873e2130d5e8c2b0b22d31ebe3299f46abe5ec7
Maksura Alam <maksura.alam@gmail.com>
2019-07-31 23:14:34 +0700

    Added state and loadQuiz function
    The function sets the states to the states in the data

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,15 +1,31 @@
 import React, { Component } from "react";
+import { quizData } from "./quizData";
 
 class Quiz extends Component {
   // Data
-
+  state = {
+    currentQuestion: 0,
+    myAnswer: null,
+    options: [],
+    score: 0,
+    disabled: true,
+    isEnd: false
+  };
   // Functions
+  loadQuizData = () => {
+    // console.log(quizData[0].question)
+    this.setState(() => {
+      return {
+        questions: quizData[this.state.currentQuestion].question,
+        answer: quizData[this.state.currentQuestion].answer,
+        options: quizData[this.state.currentQuestion].options
+      };
+    });
+  };
 
   // Render
   render() {
-    return (
-      
-    );
+    return <div className="quiz"></div>;
   }
 }
 
